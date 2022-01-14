a8796fd3e4f0b546c0fcc2a6860fcc8bbfce1675
Marina Abovyan <marrina.abovyan@gmail.com>
2022-01-14 18:45:19 +0300

    env&bodyparser

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 
+console.log(process.env.NODE_ENV)
 const { port = 3000 } = process.env;
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost:27017/moviesdb', { useNewUrlParser: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
