eeee0779c8ccbf5cd742926ed8cd9feab2de15f3
Marina Abovyan <marrina.abovyan@gmail.com>
2022-01-21 14:30:35 +0300

    fix: .env added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 
+console.log(process.env.NODE_ENV);
 const { port = 3000 } = process.env;
 const app = express();
 const mongoose = require('mongoose');
